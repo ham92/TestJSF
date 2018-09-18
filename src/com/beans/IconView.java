d6228c6dfb419333cf9f6b3138508c0a593f1d6f
ETECH-SYSTEMS\hmubaslat <hmubaslat@etech-system.com>
2018-09-18 11:02:45 +0300

    18/9
    18/9

---
+++ src/com/beans/IconView.java
@@ -0,0 +1,30 @@
+package com.beans;
+
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import org.primefaces.model.TreeNode;
+//import org.primefaces.showcase.service.DocumentService;
+
+@ManagedBean(name = "treeIconView")
+public class IconView {
+
+	private TreeNode root;
+
+	@ManagedProperty("#{documentService}")
+	private DocumentService service;
+
+	@PostConstruct
+	public void init() {
+		root = service.createDocuments();
+	}
+
+	public void setService(DocumentService service) {
+		this.service = service;
+	}
+
+	public TreeNode getRoot() {
+		return root;
+	}
+}
